16d9e3961a973b4dc627f25576ffc9148c23cd6b
LSChyi <alan81920@gmail.com>
2015-10-24 10:58:37 +0800

    update about page data modified type

--- js/modify.js
+++ js/modify.js
@@ -1,14 +1,19 @@
 angular.module('modify', [])
     .controller('modifyCtrl', function($scope, $http) {
         $scope.members = [];
-        
-        $scope.retrieve_data = function() {
+        $scope.about = {};
+
+        $scope.init_modify = function() {
+            $('.menu .item').tab();
+        }
+
+        $scope.retrieve_members = function() {
             $http.get('data/members.html')
                 .success(function(res) {
                     $scope.members = res;
                 })
                 .error(function() {
-                    alert("錯誤發生, 請聯絡管理員")
+                    alert("取得成員資料錯誤發生, 請聯絡管理員")
                 })
         }
 
@@ -32,10 +37,33 @@ angular.module('modify', [])
                     link.setAttribute("download", "members.html");
                     content += angular.toJson($scope.members);
                     break;
+                case 'about':
+                    link.setAttribute("download", "about.html");
+                    var about = angular.copy($scope.about);
+                    about.teacher.resume.experience = about.teacher.resume.experience.split("\n");
+                    about.teacher.resume.speciality = about.teacher.resume.speciality.split("\n");
+                    about.teacher.resume.course = about.teacher.resume.course.split("\n");
+                    about.teacher.resume.research = about.teacher.resume.research.split("\n");
+                    content += angular.toJson(about);
+                    break;
             }
 
             var encodedUri = encodeURI(content);
             link.setAttribute("href", encodedUri);
             link.click();
+        }
+
+        $scope.retrieve_about = function() {
+            $http.get('data/about.html')
+                .success(function(res) {
+                    $scope.about = res;
+                    $scope.about.teacher.resume.experience = $scope.about.teacher.resume.experience.toString().replace(/,/g, "\n");
+                    $scope.about.teacher.resume.speciality = $scope.about.teacher.resume.speciality.toString().replace(/,/g, "\n");
+                    $scope.about.teacher.resume.course = $scope.about.teacher.resume.course.toString().replace(/,/g, "\n");
+                    $scope.about.teacher.resume.research = $scope.about.teacher.resume.research.toString().replace(/,/g, "\n");
+                })
+                .error(function() {
+                    alert("去得about頁面資料發生錯誤，請聯絡管理員")
+                })
         }
     })
