30f7eb85698b7b40d3db9ccd93c4039558f45faa
LSChyi <alan81920@gmail.com>
2015-11-21 11:43:35 +0800

    modify about

--- js/about.js
+++ js/about.js
@@ -3,11 +3,9 @@ angular.module('about', [])
         $scope.teacher = {};
 
         $scope.init_about = function() {
-            $('.menu .item').tab();
             $http.get('data/about.html')
                 .success(function(res) {
                     $scope.teacher = res.teacher;
-                    console.log(res);
                 })
                 .error(function() {
                     alert('取得頁面資料錯誤，請聯絡管理員');
